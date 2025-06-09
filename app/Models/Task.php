0e3816ac4c38c478dfa5ef5cb6f37056986ad78a
Pedro Duarte <pdr.maia.d@gmail.com>
2025-06-09 19:47:49 +0000

    configuração das entidades

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,10 +1,24 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'completed',
+        'todo_list_id',
+    ];
+
+    protected $casts = [
+        'completed' => 'boolean',
+    ];
+
+    public function todoList(): BelongsTo
+    {
+        return $this->belongsTo(TodoList::class);
+    }
 }
